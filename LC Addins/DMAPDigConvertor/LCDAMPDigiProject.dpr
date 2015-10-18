7449fd63de6925be779b807d7d6fa6ff7a7d0828
mikeweideli <mike@lfield.co.uk>
2015-10-18 21:46:53 +0100

    Dmap Digitiser Addin
    Changes required for new repository

--- LC Addins/DMAPDigConvertor/LCDAMPDigiProject.dpr
+++ LC Addins/DMAPDigConvertor/LCDAMPDigiProject.dpr
@@ -4,7 +4,7 @@ uses
   ComServ,
   LCDAMPDigiProject_TLB in 'LCDAMPDigiProject_TLB.pas',
   LCDMAPDigiForm1Impl1 in 'LCDMAPDigiForm1Impl1.pas' {LCDMAPDigiForm1: TActiveForm} {LCDMAPDigiForm1: CoClass},
-  Recorder2000_TLB in '..\Imports\Recorder2000_TLB.pas';
+  Recorder2000_TLB in '..\..\Third Party\Imports\Recorder2000_TLB.pas';
 
 {$E ocx}
 
